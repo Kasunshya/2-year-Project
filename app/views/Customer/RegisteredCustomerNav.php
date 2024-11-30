7db74658dd3f9e14b5ddb76d0dcf8f025043b5e4
Umeshi2002 <160719142+Umeshi2002@users.noreply.github.com>
2024-11-30 15:23:41 +0530

    Add files via upload
    created a folder with files

---
+++ app/views/Customer/RegisteredCustomerNav.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Bakery Website</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../public/css/Customer/customerhomepage.css">
+</head>
+
+<div class="navbar">
+    <a href="#" class="logo">FROSTINE</a>
+    <ul>
+        <li><a href="<?php echo URLROOT?>/customer/customerhomepage">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#product">Product</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#review">Review</a></li>
+        <li><a href="#customization" style="color: #c98d83; font-weight: bold; text-decoration: none;">Customization</a></li>
+        <li><a href="#profile">Profile</a></li>
+    </ul>
+</div>
