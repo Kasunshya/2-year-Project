a0d440af0843f4f5aafcefcd22ef598ceadcf6e1
Kasunshya Pabodi Jayawardana <kasunshyapabodi2002@gmail.com>
2024-11-30 09:57:35 +0530

    resolve conflicts

--- app/models/M_Cashier.php
+++ app/models/M_Cashier.php
@@ -44,9 +44,6 @@
     return $this->db->execute();
 }*/
 
-
-
-
 public function getCashiers() {
     $this->db->query("SELECT cashier_id, cashier_name, address, contacts, join_date, branch_name 
                       FROM cashier");
